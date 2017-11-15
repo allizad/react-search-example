63d960fb6be7fd5207211dbf9d5554512b26e1e9
Allison Zadrozny <allison@zadrozny.com>
2017-11-14 21:30:10 -0600

    Add event handlers and turn component into a controlled component.

--- modules/search.js
+++ modules/search.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+const ELASTICSEARCH_URL = "https://shakestat-oregon-9345328632.us-west-2.bonsaisearch.net";
+
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  handleInputChange(e) {
+    this.setState({query: e.target.value});
+  }
+
+  onClick() {
+    console.log(`search for ${this.state.query}`);
+  }
+
   render(){
     return (
       <div className="_Search">
-        <input type="text" value="search"/>
-        <button>Submit</button>
+        <input type="text" name="query" value={this.state.q} onChange={this.handleInputChange.bind(this)}/>
+        <button onClick={this.onClick.bind(this)}>Submit</button>
       </div>
     )
   }
