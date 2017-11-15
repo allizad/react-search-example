05adc5454c4478af87423b3835d7a7b389787e1d
Allison Zadrozny <allison@zadrozny.com>
2017-11-14 21:39:29 -0600

    Add failing query.

--- modules/search.js
+++ modules/search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'whatwg-fetch';
 
 const ELASTICSEARCH_URL = "https://shakestat-oregon-9345328632.us-west-2.bonsaisearch.net";
 
@@ -14,8 +15,34 @@ class Search extends React.Component {
     this.setState({query: e.target.value});
   }
 
+  basicQuery() {
+    return {
+      "query" : {
+        "bool" : {
+          "must" : [
+            { "match" : { "title" : this.state.query } }
+          ]
+        }
+      }
+    }
+  }
+
   onClick() {
     console.log(`search for ${this.state.query}`);
+    let url = `${ELASTICSEARCH_URL}/_search`;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.basicQuery())
+    }).then(function(response) {
+      return response.json()
+    }).then(function(json) {
+      console.log('parsed json', json)
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+    });
   }
 
   render(){
