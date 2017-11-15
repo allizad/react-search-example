b63a2c43ea6b4f6c0dc50b3b247e21712bdbc5de
Allison Zadrozny <allison@zadrozny.com>
2017-11-14 21:21:41 -0600

    Add react and basic search form field

--- client/index.js
+++ client/index.js
@@ -1,1 +1,20 @@
 console.log('some stuff is here.')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from '../modules/search';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="_App">
+        <Search/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
---
+++ modules/search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render(){
+    return (
+      <div className="_Search">
+        <input type="text" value="search"/>
+        <button>Submit</button>
+      </div>
+    )
+  }
+}
+
+export default Search;
